4f30c982435d9bdb282fd84e01f8999f487035fb
skrnoob <a775463931@163.com>
2020-10-28 23:02:12 +0800

    .

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
-        println!("{CASE_INSENSITIVE: }",case_sensitive);
+        println!("CASE_INSENSITIVE: {}",case_sensitive);
         Ok(Config { query, filename, case_sensitive })
 
     }
